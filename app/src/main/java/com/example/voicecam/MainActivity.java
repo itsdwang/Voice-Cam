f7a0b6297a971b40b8e3029ffc35141302a07bd0
dberumen <dberumen@ucsc.edu>
2019-05-08 22:36:35 -0700

    phrase "take a picture" now opens camera

--- app/src/main/java/com/example/voicecam/MainActivity.java
+++ app/src/main/java/com/example/voicecam/MainActivity.java
@@ -79,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
 
                 if(matches != null) {
                     editText.setText(matches.get(0));
+                    if(matches.get(0).equals("take a picture")){
+                        Intent intent = new Intent(MainActivity.this, CameraTestActivity.class);
+                        intent.putExtra("message", "take a picture");
+                        startActivity(intent);
+                    }
                 }
             }
 
