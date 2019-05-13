3493a23ab58a991a06b6aa602d5b23ee766a3b29
Daniel Wang <itsdwang@gmail.com>
2019-05-12 21:45:33 -0700

    Added full screen image view, zoom functionality

--- app/src/main/java/com/example/voicecam/DisplayImagesActivity.java
+++ app/src/main/java/com/example/voicecam/DisplayImagesActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +18,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -68,82 +72,104 @@ public class DisplayImagesActivity extends AppCompatActivity {
         imgAdapter = new ImageAdapter(this, R.layout.grid_item_layout, arrList);
         gridView.setAdapter(imgAdapter);
 
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            // ImageView imageView = (ImageView) findViewById(R.id.rowImage);
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+                ImageItem i = (ImageItem) parent.getItemAtPosition(position);
+                Bitmap bmp = i.getImage();
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.JPEG, 60, stream);
+                byte[] byteArray = stream.toByteArray();
+                Intent intent = new Intent(DisplayImagesActivity.this, FullImageActivity.class);
+
+                intent.putExtra("picture", byteArray);
+                startActivity(intent);
+
+                return true;
+            }
+        });
+
+
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            ImageItem i = (ImageItem) parent.getItemAtPosition(position);
-            final File f = i.getFile();
+                ImageItem i = (ImageItem) parent.getItemAtPosition(position);
+                final File f = i.getFile();
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(DisplayImagesActivity.this);
-            builder.setTitle("Options");
-            builder.setItems(options, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                if ("Rename".equals(options[which])){
-                    Log.d(this.getClass().getSimpleName(), "Rename clicked");
+                AlertDialog.Builder builder = new AlertDialog.Builder(DisplayImagesActivity.this);
+                builder.setTitle("Options");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if ("Rename".equals(options[which])){
+                            Log.d(this.getClass().getSimpleName(), "Rename clicked");
 
-                    // Create another action dialog
-                    AlertDialog.Builder nameBuilder = new AlertDialog.Builder(DisplayImagesActivity.this);
-                    nameBuilder.setTitle("Rename Photo");
+                            // Create another action dialog
+                            AlertDialog.Builder nameBuilder = new AlertDialog.Builder(DisplayImagesActivity.this);
+                            nameBuilder.setTitle("Rename Photo");
 
-                    final EditText input = new EditText(DisplayImagesActivity.this);
-                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
-                    nameBuilder.setView(input);
+                            final EditText input = new EditText(DisplayImagesActivity.this);
+                            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                            nameBuilder.setView(input);
 
-                    nameBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            renamedFile = input.getText().toString();
-                            Log.d(this.getClass().getSimpleName(), "Renamed File: " + renamedFile + ".jpg");
+                            nameBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                renamedFile = input.getText().toString();
+                                Log.d("Debug", "Renamed File: " + renamedFile + ".jpg");
 
-                            renameFile(f, renamedFile + ".jpg");
+                                renameFile(f, renamedFile + ".jpg");
+
+                                // Refresh activity
+                                finish();
+                                startActivity(getIntent());
+                                }
+                            });
+
+                            nameBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                            AlertDialog ad = nameBuilder.show();
+                        }
+                        else if ("Delete".equals(options[which])){
+                            Log.d("Debug", "Delete clicked");
+
+                            f.delete();
 
                             // Refresh activity
                             finish();
                             startActivity(getIntent());
                         }
-                    });
+                        else if ("Share".equals(options[which])){
+                            Log.d("Debug", "Share clicked");
 
-                    nameBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
+                            // Remove the strict mode policies
+                            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                            StrictMode.setVmPolicy(builder.build());
+
+                            Log.d(this.getClass().getSimpleName(), "Grid item clicked");
+                            Uri u = Uri.fromFile(f);
+
+                            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                            shareIntent.setType("Image/*");
+                            shareIntent.putExtra(Intent.EXTRA_STREAM, u);
+
+                            startActivityForResult(shareIntent, 1);
+                            // startActivity(shareIntent);
                         }
-                    });
-
-                    AlertDialog ad = nameBuilder.show();
-                }
-                else if ("Delete".equals(options[which])){
-                    Log.d(this.getClass().getSimpleName(), "Delete clicked");
-
-                    f.delete();
-
-                    // Refresh activity
-                    finish();
-                    startActivity(getIntent());
-                }
-                else if ("Share".equals(options[which])){
-                    Log.d(this.getClass().getSimpleName(), "Share clicked");
-
-                    // Remove the strictmode policies
-                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-                    StrictMode.setVmPolicy(builder.build());
-
-                    Log.d(this.getClass().getSimpleName(), "Grid item clicked");
-                    Uri u = Uri.fromFile(f);
-
-                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                    shareIntent.setType("Image/*");
-                    shareIntent.putExtra(Intent.EXTRA_STREAM, u);
-
-                    startActivityForResult(shareIntent, 1);
-                    // startActivity(shareIntent);
-                }
-                }
-            });
-            builder.show();
+                    }
+                });
+                builder.show();
             }
         });
+
     }
 
     public void renameFile(File f, String s) {
---
+++ app/src/main/java/com/example/voicecam/FullImageActivity.java
@@ -0,0 +1,36 @@
+package com.example.voicecam;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            byte[] byteArray = extras.getByteArray("picture");
+
+            Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            ImageView image = (ImageView) findViewById(R.id.fullImageView);
+            image.setImageBitmap(bmp);
+
+
+            PhotoViewAttacher pAttacher = new PhotoViewAttacher(image);
+            pAttacher.update();
+        } catch(NullPointerException n) {
+            n.printStackTrace();
+        }
+    }
+}
